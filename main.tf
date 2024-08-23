73e3a6e21ac2139ae3a91547a8f9224a55b8c045
박창헌님/상황관리 <P194961@sktelecom.com>
2024-08-23 13:17:37 +0900

    test

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
+  cluster_name = "tf-11426-cluster-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
