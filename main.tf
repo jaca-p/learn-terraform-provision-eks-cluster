bac22ad984602d838758228621b9abba541771f6
박창헌님/상황관리 <P194961@sktelecom.com>
2024-08-27 18:17:52 +0900

    add provider

--- main.tf
+++ main.tf
@@ -5,6 +5,18 @@ provider "aws" {
   region = var.region
 }
 
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+provider "kubectl" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
 # Filter out local zones, which are not currently supported 
 # with managed node groups
 data "aws_availability_zones" "available" {
